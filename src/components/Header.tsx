9b43e1f3adc84c5c24df200515838c9d252200ed
ttf416 <thefirstfruit416@gmail.com>
2025-02-23 04:34:15 +0900

    feat: user-main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,24 @@
-import Image from "next/image"
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const Header = () => {
-
   const router = useRouter();
-
+  const currentPath = router.pathname;
   const homeButtonClick = () => {
     router.push("/");
-  }
+  };
 
   return (
     <header className="flex ml-[205px] mt-[85px]">
-      <div className="">
+      <div className="flex">
         <button onClick={homeButtonClick}>
           <Image src="/images/logo.svg" alt="logo" width={47} height={32} />
-        </button >
+        </button>
+        <div>
+          {
+        }</div>
       </div>
-
     </header>
   );
 };
